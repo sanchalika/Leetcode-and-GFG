c628871c775cb84468ef0dfa566c2178cb5b0354
Sanchalika <sanchalika2019@gmail.com>
2022-03-01 10:26:23 +0530

    Time: 14 ms (16.41%), Space: 7.7 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>mem(n+1);
         mem[0]=0;
         for(int i=1;i<=n;i++){
-            mem[i]=mem[i/2] + i%2;
+            mem[i]=mem[i/2] + i%2; // x/2=y, for even no. number of set bits in x is same and in odd numbers its 1+x
         }
         return mem;
         
