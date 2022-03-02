20328384cd8aa361d4e21cf49edb898bf51690c2
Sanchalika <sanchalika2019@gmail.com>
2022-03-02 22:56:29 +0530

    Time: 3 ms (47.29%), Space: 8 MB (10.71%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,15 +1,55 @@
 class Solution {
 public:
-     int k=0;
+     // int k=0;
+      int lcs(string A, string B, int n,int m){
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(A[i-1]==B[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else 
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                
+            }
+        }
+        return dp[n][m];
+    }
     bool isSubsequence(string s, string t) {
-        for(int i=0;i<t.length();i++){
-            if(t[i]==s[k])
-                k++;
-        }
-        if(k==s.length()){
+      
+        if(lcs(s,t,s.length(),t.length())==s.length())
             return true;
-        }
-        return false;
+        else 
+            return false;
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // for(int i=0;i<t.length();i++){
+  //           if(t[i]==s[k])
+  //               k++;
+  //       }
+  //       if(k==s.length()){
+  //           return true;
+  //       }
+  //       return false;
