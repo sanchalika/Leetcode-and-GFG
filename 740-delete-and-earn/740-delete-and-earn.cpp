8c83eccc1ec18f735309dabf662dfc9433749a95
Sanchalika <sanchalika2019@gmail.com>
2022-03-05 10:43:46 +0530

    Time: 4 ms (94.57%), Space: 9.3 MB (83.08%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[20001];
+    int solve(vector<int>&nums, int pos){
+        if(pos>=nums.size()) return 0;
+        if(dp[pos]!=-1) return dp[pos];
+        
+        int curr=nums[pos];
+        int sum=nums[pos];
+        int idx=pos+1;
+        
+        while(idx<nums.size() && nums[idx]==curr){
+            sum+=nums[idx];
+            idx++;
+        }
+        while(idx<nums.size() && nums[idx]==curr+1){
+            idx++;
+        }
+        return dp[pos] = max(sum+solve(nums, idx), solve(nums, pos+1));
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        memset(dp,-1,sizeof(dp));
+        return solve(nums, 0);
+    }
+};
