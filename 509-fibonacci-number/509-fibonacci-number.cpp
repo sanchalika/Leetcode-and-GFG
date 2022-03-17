41f38e7b01d68ad320692453c8531db6ace091ad
Sanchalika <sanchalika2019@gmail.com>
2022-03-17 16:17:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (40.76%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[31];
+    int calculate(int n){
+        if(n==0 || n==1){
+            return dp[n]=n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=calculate(n-1)+calculate(n-2);
+    }
+    int fib(int n) {
+        memset(dp,-1,sizeof(dp));
+        int ans=calculate(n);
+        return ans;
+    }
+};
