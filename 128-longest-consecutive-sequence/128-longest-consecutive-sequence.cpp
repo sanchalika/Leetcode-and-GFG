2c4ebf1bc417e3655431ff4df47c168516e75783
Sanchalika <sanchalika2019@gmail.com>
2022-05-24 14:27:19 +0530

    Time: 806 ms (15.40%), Space: 49.9 MB (10.24%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
 int longestConsecutive(vector<int>& nums) {
-unordered_set<int>hashSet;
-for(auto i : nums){
-hashSet.insert(i);
-}
-
-    int longestStreak = 0 ; //it stores max of all consecutive sequence
-    for(auto i : nums){
-        if(!hashSet.count(i-1)){
-            int currentNum = i;
-            int currentStreak = 1; //this is the length of the iteratio we initialise it with 1
-            while(hashSet.count(currentNum+1)){  //we check if next number exist or not like for 100 we check if 101 exist or not
-                currentNum+=1; //if it does we increase
-                currentStreak+=1; //and we increase the counter pointer which counts the number of iteration
+        unordered_set<int>hash;
+        
+        int lcs = 0;
+        
+        for(int it : nums)
+            hash.insert(it);
+        
+        for(int it : nums)
+        {
+            if(!hash.count(it-1))
+            {
+                int curNum = it;
+                int streak = 1;
+                
+                while(hash.count(curNum+1))
+                {
+                    curNum += 1;
+                    streak += 1;
+                }
+                lcs = max(lcs,streak);
             }
-            longestStreak = max(longestStreak , currentStreak);
         }
+        return lcs;
     }
-    return longestStreak;
-}
 };
