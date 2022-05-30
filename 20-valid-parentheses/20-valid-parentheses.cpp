34aa24c194e1ac72198b7313819f1f29626b4fb8
Sanchalika <sanchalika2019@gmail.com>
2022-05-30 20:09:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.84%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -4,13 +4,12 @@ public:
         stack<char> st;
         for(int i=0;i<s.size();i++){
             if(s[i]=='(' || s[i]=='[' || s[i]=='{')
-            {st.push(s[i]);continue;}
-            
-            
+            {
+                st.push(s[i]);
+                continue;
+            }
             if(st.empty())
                 return false;
-            
-            
             if(s[i]==')'){
                 if(st.top()=='('){
                     st.pop();
