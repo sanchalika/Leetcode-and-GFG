735839bb623593cd63ebf6a6d91dc7c24f5bae97
Sanchalika <sanchalika2019@gmail.com>
2022-06-11 16:17:06 +0530

    Time: 63 ms (24.93%), Space: 27.6 MB (13.32%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+    // int bsearch(vector<int>& nums, int target,int low,int high){
+    //     int mid=(low+high)/2;
+    //     if(low>high) return -1;
+    //     if(nums[mid]==target) return mid;
+    //     else if(nums[mid]>target)
+    //         return bsearch(nums,target, low,mid-1);
+    //     else
+    //         return bsearch(nums, target, mid+1,high);
+    // }
     int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)
-                return i;
-        }
-        return -1;
+int n = nums.size()-1;
+int low = 0, high = n;
+while( low <= high){
+int mid = low + (high-low)/2;
+if (nums[mid] == target) return mid;
+else if (nums[mid] > target) high = mid -1;
+else low = mid + 1;
+}
+return -1;
     }
 };
