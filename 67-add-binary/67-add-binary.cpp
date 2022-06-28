f3f6189ed175e05c57841ec5ff0d7214a16c721e
Sanchalika <sanchalika2019@gmail.com>
2022-06-28 17:43:50 +0530

    Time: 7 ms (30.66%), Space: 6.3 MB (56.56%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
 string addBinary(string a, string b) {
-string res = "";
-int temp=0;
-int m = a.size()-1;
-int n = b.size()-1;
-while(m>=0 || n>=0 || temp==1){
-temp+=((m>=0)?a[m]-'0':0);
-temp+=((n>=0)?b[n]-'0':0);
-res = char(temp%2+'0')+res;
-temp/=2;
-m--,n--;
-}
-return res;
-}
+    int i=a.length()-1;  
+    int j=b.length()-1;
+    int carry=0;
+    string ans="";
+    
+    while(i>=0 || j>=0){
+        
+        int sum=carry;              
+        if(i>=0) sum+= a[i--]-'0';  
+        if(j>=0) sum+= b[j--]-'0';
+        
+        carry=sum/2;
+        int total=sum%2;
+        ans+=to_string(total);
+        
+    }
+    
+    if(carry) ans+=to_string(carry);
+    
+    reverse(ans.begin(),ans.end());
+    
+    return ans;
+    
+    }
 };
