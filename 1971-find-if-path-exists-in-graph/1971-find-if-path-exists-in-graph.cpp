af47ac87848fcc3fd6f9137b3722e7d9c6ac323b
Sanchalika <sanchalika2019@gmail.com>
2022-07-02 12:19:50 +0530

    Time: 2035 ms (5.01%), Space: 225.6 MB (18.41%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
-    {
-        // do a bfs
-        vector<bool> visited(n, false);
-        vector<vector<int>> adjList(n, vector<int>());
-        
-        for(vector<int> & edge: edges)
-        {
-            adjList[edge[0]].push_back(edge[1]);
-            adjList[edge[1]].push_back(edge[0]);
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        unordered_map<int, vector<int>>mp;
+        for(auto x:edges){
+            vector<int>temp=x;
+            int u=temp[0];
+            int v=temp[1];
+            
+            mp[u].push_back(v);
+            mp[v].push_back(u);
         }
         
-        queue<int> Q;
-        Q.push(source);
-        visited[source] = true;
+        vector<bool>vis(n+1,false);
+        queue<int>q;
+        q.push(start);
+        vis[start]=true;
         
-        while(Q.size())
-        {
-            int node = Q.front(); Q.pop();
-            
-            if(node == destination)
-                return true;
-            
-            for(int neighbor: adjList[node])
-            {
-                if(visited[neighbor] == false)
-                {
-                    visited[neighbor] = true;
-                    Q.push(neighbor);
+        while(!q.empty()){
+            int v=q.front();
+            q.pop();
+            vector<int>temp=mp[v];
+                for(int i=0;i<temp.size();i++){
+                    int vertex=temp[i];
+                    if(vis[vertex]==false){
+                        q.push(vertex);
+                        vis[vertex]=true;
+                    }
                 }
-            }
         }
-        
-        return visited[destination];
+        return vis[end];
     }
 };
