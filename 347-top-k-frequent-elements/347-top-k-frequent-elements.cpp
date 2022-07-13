f7e1f6d591907ea57e2af07881ba700f9e2ae6dc
Sanchalika <sanchalika2019@gmail.com>
2022-07-14 01:04:15 +0530

    Time: 23 ms (61.12%), Space: 13.7 MB (43.43%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -18,3 +18,31 @@ public:
         return res;
     }
 };
+
+
+
+// vector<int> topKFrequent(vector<int>& nums, int k) {
+//         int n=nums.size();
+        
+//         vector<int>res;
+        
+//         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minh;
+        
+//         unordered_map<int,int>mp;
+//         for(int i=0;i<n;i++){
+//             mp[nums[i]]++;
+//         }
+        
+//         for(auto i=mp.begin();i<mp.end();i++){
+            
+//             minh.push({i.second,i.first});
+//             if(minh.size()>0){
+//                 minh.pop();
+//             }
+//         }
+        
+//         while(minh.size()>0){
+//             res.push_back(mp.top().second);
+//         }
+        
+//         return res;
