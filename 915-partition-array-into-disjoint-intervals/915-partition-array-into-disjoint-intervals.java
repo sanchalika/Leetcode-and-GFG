b03f87c73737f08a2d5ea9a5a675dfe325fce401
Sanchalika <sanchalika2019@gmail.com>
2022-07-17 00:18:09 +0530

    Time: 3 ms (92.19%), Space: 86.9 MB (79.05%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -3,50 +3,54 @@
 // 3) Figure out the situation when leftmax < rightmin of one step ahead of leftmax
 
 
-//-------------------------------------------------------------------------------------//
+//-----------------------------------  O(n) time complexity  --------------------------------------------------//
 
+
+// class Solution {
+//     public int partitionDisjoint(int[] arr) {
+//         //1. make a right min arr
+//         int n=arr.length;
+//         int[] rightMin=new int[n+1];
+        
+//         rightMin[n]=Integer.MAX_VALUE;
+//         for(int i=n-1;i>=0;i--){
+//             rightMin[i]=Math.min(rightMin[i+1], arr[i]);
+//         }
+        
+//         //2) Traverse on array and maintain leftmax and figure out the partition index
+//         int leftMax=Integer.MIN_VALUE;
+//         //int ans=0;
+        
+//         for(int i=0;i<n;i++){
+            
+//             leftMax=Math.max(leftMax, arr[i]);
+//             if(leftMax <= rightMin[i+1]){
+//                 return i+1;
+//             }
+                
+//         }
+//         return -1;
+        
+//     }
+// }
+
+//----------------------------   O(1) ---------------------------------------//
 
 class Solution {
     public int partitionDisjoint(int[] arr) {
-        //1. make a right min arr
-        int n=arr.length;
-        int[] rightMin=new int[n+1];
+    
+        int leftMax=arr[0];
+        int greater=arr[0];
         
-        rightMin[n]=Integer.MAX_VALUE;
-        for(int i=n-1;i>=0;i--){
-            rightMin[i]=Math.min(rightMin[i+1], arr[i]);
-        }
-        
-        //2) Traverse on array and maintain leftmax and figure out the partition index
-        int leftMax=Integer.MIN_VALUE;
-        //int ans=0;
-        
-        for(int i=0;i<n;i++){
-            
-            leftMax=Math.max(leftMax, arr[i]);
-            if(leftMax <= rightMin[i+1]){
-                return i+1;
+        int ans=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>greater){
+                greater=arr[i];
+            }else if(arr[i]<leftMax){
+                leftMax=greater;
+                ans=i;
             }
-                
-        }
-        return -1;
-        
+        }        
+        return ans+1;
     }
 }
-// class Solution {
-//     public int partitionDisjoint(int[] arr) {
-//         int rmin[]=new int[arr.length+1];
-//     rmin[arr.length]=Integer.MAX_VALUE;
-//     for(int i=arr.length-1;i>=0;i--){
-//       rmin[i]=Math.min(rmin[i+1],arr[i]);
-//     }
-//     int lmax=Integer.MIN_VALUE;
-//     for(int i=0;i<arr.length;i++){
-//       lmax=Math.max(lmax,arr[i]);
-//       if(lmax<=rmin[i+1]){
-//         return i+1;
-//       }
-//     }
-//     return -1;
-//     }
-// }
