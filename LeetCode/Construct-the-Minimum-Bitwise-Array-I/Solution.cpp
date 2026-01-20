6e237b3752afce69ff7912bc2b105b74e797020f
Sanchalika <sanchalika2019@gmail.com>
2026-01-20 15:18:17 +0530

    Add solution for Construct the Minimum Bitwise Array I

---
+++ LeetCode/Construct-the-Minimum-Bitwise-Array-I/Solution.cpp
@@ -0,0 +1,6 @@
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        for (int& n : nums) {
+            n = n ^ (((n + 1) & ~n) >> 1) | ((n & 1) - 1);
+        }
+        return move(nums);
+    }
