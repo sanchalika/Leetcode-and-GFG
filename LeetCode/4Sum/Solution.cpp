c342df02f29790008ee7b4b83b4430cddbac1c0d
Sanchalika <sanchalika2019@gmail.com>
2026-02-22 23:11:14 +0530

    Add solution for 4Sum

--- LeetCode/4Sum/Solution.cpp
+++ LeetCode/4Sum/Solution.cpp
@@ -2,24 +2,34 @@
 2public:
 3    vector<vector<int>> fourSum(vector<int>& nums, int target) {
 4        int n = nums.size();
-5        set<vector<int>> res;
+5        vector<vector<int>> ans;
 6        sort(nums.begin(), nums.end());
 7        for (int i = 0; i < n; i++) {
-8            for (int j = i + 1; j < n; j++) {
-9                set<long long> st;
-10                for (int k = j + 1; k < n; k++) {
-11                    long long sum = (long long)nums[i] + nums[j] + nums[k];
-12                    long long fourth = (long long)target - sum;
-13                    if (st.find(fourth) != st.end()) {
-14                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-15                        sort(temp.begin(), temp.end());
-16                        res.insert(temp);
-17                    }
-18                    st.insert(nums[k]);
-19                }
-20            }
-21        }
-22        vector<vector<int>> ans(res.begin(), res.end());
-23        return ans;
-24    }
-25};
+8            if (i > 0 && nums[i] == nums[i - 1])
+9                continue;
+10            for (int j = i + 1; j < n; j++) {
+11                if (j > i + 1 && nums[j] == nums[j - 1])
+12                    continue;
+13                int k = j + 1, l = n - 1;
+14                while (k < l) {
+15                    long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
+16                    if (sum < target) {
+17                        k++;
+18                    } else if (sum > target) {
+19                        l--;
+20                    } else {
+21                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+22                        ans.push_back(temp);
+23                        k++;
+24                        l--;
+25                        while (k < l && nums[k] == nums[k - 1])
+26                            k++;
+27                        while (k < l && nums[l] == nums[l + 1])
+28                            l--;
+29                    }
+30                }
+31            }
+32        }
+33        return ans;
+34    }
+35};
