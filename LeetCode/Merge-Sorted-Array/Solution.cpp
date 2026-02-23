e19ba75861ef65a1e6a12d234b35c00948b60388
Sanchalika <sanchalika2019@gmail.com>
2026-02-23 18:30:23 +0530

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.cpp
+++ LeetCode/Merge-Sorted-Array/Solution.cpp
@@ -1,21 +1,37 @@
 1class Solution {
-2public:
-3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-4        int left = m - 1;
-5        int right = 0;
-6        while (left >= 0 && right < n) {
-7            if (nums1[left] > nums2[right]) {
-8                swap(nums1[left], nums2[right]);
-9                left--;
-10                right++;
-11            } else {
-12                break;
-13            }
-14        }
-15        sort(nums1.begin(), nums1.begin() + m);
-16        sort(nums2.begin(), nums2.begin() + n);
-17        for (int i = 0; i < n; i++) {
-18            nums1[m + i] = nums2[i];
-19        }
-20    }
-21};
+2private:
+3    void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int idx1,
+4                       int idx2) {
+5        if (nums1[idx1] > nums2[idx2]) {
+6            swap(nums1[idx1], nums2[idx2]);
+7        }
+8    }
+9
+10public:
+11    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+12        int len = (n + m);
+13        int gap = (len / 2) + (len % 2);
+14        while (gap > 0) {
+15            int left = 0;
+16            int right = left + gap;
+17            while (right < len) {
+18                if (left < m && right >= m) {
+19                    swapIfGreater(nums1, nums2, left, right - m);
+20                } else if (left >= m) {
+21                    swapIfGreater(nums2, nums2, left - m, right - m);
+22                } else {
+23                    swapIfGreater(nums1, nums1, left, right);
+24                }
+25
+26                left++;
+27                right++;
+28            }
+29            if (gap == 1)
+30                break;
+31            gap = (gap / 2) + (gap % 2);
+32        }
+33        for (int i = 0; i < n; i++) {
+34            nums1[m + i] = nums2[i];
+35        }
+36    }
+37};
