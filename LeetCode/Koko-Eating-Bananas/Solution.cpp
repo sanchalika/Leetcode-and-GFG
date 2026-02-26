68a5ac32f847fb29c79b57dee80081b6e7a94f1c
Sanchalika <sanchalika2019@gmail.com>
2026-02-26 12:48:41 +0530

    Add solution for Koko Eating Bananas

--- LeetCode/Koko-Eating-Bananas/Solution.cpp
+++ LeetCode/Koko-Eating-Bananas/Solution.cpp
@@ -1,14 +1,14 @@
 1class Solution {
-2private:
-3    int findMax(vector<int>& v){
-4        int maxi=INT_MIN;
-5        int n=v.size();
-6        for(int i=0; i<n; i++){
-7            maxi=max(maxi, v[i]);
+2public:
+3    int findMax(vector<int>& piles) {
+4        int maxi = INT_MIN;
+5        int n = piles.size();
+6        for (int i = 0; i < n; i++) {
+7            maxi = max(maxi, piles[i]);
 8        }
 9        return maxi;
 10    }
-11    long long calculateHours(vector<int>& v, int hourly){
+11    long long calculateTotalHrs(vector<int>& v, int hourly){
 12        long long totalH=0;
 13        int n=v.size();
 14        for(int i=0; i<n; i++){
@@ -16,19 +16,18 @@
 16        }
 17        return totalH;
 18    }
-19public:
-20    int minEatingSpeed(vector<int>& v, int h) {
-21        int low=1, high=findMax(v);
-22        while(low<=high){
-23            int mid=low+(high-low)/2;
-24            long long totalH=calculateHours(v, mid);
-25            if(totalH<=h){
-26                high=mid-1;
-27            }
-28            else{
-29                low=mid+1;
-30            }
-31        }
-32        return low;
-33    }
-34};
+19    int minEatingSpeed(vector<int>& piles, int h) {
+20        int low = 1;
+21        int high = findMax(piles);
+22        while (low <= high) {
+23            int mid = low + (high - low) / 2;
+24            long long totalHrs = calculateTotalHrs(piles, mid);
+25            if (totalHrs <= h) {
+26                high = mid - 1;
+27            } else {
+28                low = mid + 1;
+29            }
+30        }
+31        return low;
+32    }
+33};
