8eec25b12fd30a89df59a854be8b0acc6668aaa9
Sanchalika <sanchalika2019@gmail.com>
2026-02-27 13:57:07 +0530

    Add solution for Capacity To Ship Packages Within D Days

--- LeetCode/Capacity-To-Ship-Packages-Within-D-Days/Solution.cpp
+++ LeetCode/Capacity-To-Ship-Packages-Within-D-Days/Solution.cpp
@@ -1,40 +1,45 @@
-class Solution {
-public:
-    void findMaxNSum(vector<int>& weights, int &low, int &high){
-        for(int i = 0; i < weights.size();i++){
-            if (low > weights[i]) low = weights[i];
-            high += weights[i];
-        }
-    }
-
-    bool findMinWeights(vector<int>& weights, int days, int minWeight){
-        int temp = minWeight;
-        int i = 0;
-        while (i < weights.size() && days>0){
-            if (temp < weights[i]){
-                days -= 1;
-                temp = minWeight;
-            }
-            else {
-                temp -= weights[i];
-                i += 1;
-            }
-        }
-        return (i == weights.size());
-        
-    }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low =0, high = 0;
-        int ans = 0;
-        findMaxNSum(weights, low, high);
-        while (low<=high){
-            int mid = (low+high) / 2;
-            if (findMinWeights(weights, days, mid)){
-                ans = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    int findMax(vector<int>& weights, int n) {
+4        int maxi = INT_MIN;
+5        for (int i = 0; i < n; i++) {
+6            maxi = max(maxi, weights[i]);
+7        }
+8        return maxi;
+9    }
+10    int sumOfWeights(vector<int>& weights, int n) {
+11        int total = 0;
+12        for (int i = 0; i < n; i++) {
+13            total += weights[i];
+14        }
+15        return total;
+16    }
+17    int noOfDays(vector<int>& weights, int capacity) {
+18        int day = 1;
+19        int load = 0;
+20        int n = weights.size();
+21        for (int i = 0; i < n; i++) {
+22            if (load + weights[i] > capacity) {
+23                day = day + 1;
+24                load = weights[i];
+25            } else {
+26                load += weights[i];
+27            }
+28        }
+29        return day;
+30    }
+31    int shipWithinDays(vector<int>& weights, int days) {
+32        int n = weights.size();
+33        int low = findMax(weights, n);
+34        int high = sumOfWeights(weights, n);
+35        while (low <= high) {
+36            int mid = low + (high - low) / 2;
+37            if (noOfDays(weights, mid) <= days) {
+38                high = mid - 1;
+39            } else {
+40                low = mid + 1;
+41            }
+42        }
+43        return low;
+44    }
+45};
