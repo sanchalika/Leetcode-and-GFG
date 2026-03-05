cb068dc2e3a41a1018687d36ba2dda96d513a3d8
Sanchalika <sanchalika2019@gmail.com>
2026-03-05 16:41:31 +0530

    Add solution for Reverse Words in a String

--- LeetCode/Reverse-Words-in-a-String/Solution.cpp
+++ LeetCode/Reverse-Words-in-a-String/Solution.cpp
@@ -1,25 +1,20 @@
 1class Solution {
 2public:
 3    string reverseWords(string s) {
-4       reverse(s.begin(), s.end());
-5        int n = s.size();
-6        int left = 0;
-7        int right = 0;
-8        int i = 0;
-9        while (i < n) {
-10            while (i < n && s[i] == ' ')
-11                i++;
-12            if (i == n)
-13                break;
-14            while (i < n && s[i] != ' ') {
-15                s[right++] = s[i++];
-16            }
-17            reverse(s.begin() + left, s.begin() + right);
-18            s[right++] = ' ';
-19            left = right;
-20            i++;
-21        }
-22        s.resize(right - 1);
-23        return s; 
-24    }
-25};
+4       int n=s.length();
+5       string ans="";
+6       reverse(s.begin(), s.end());
+7       for(int i=0; i<n; i++){
+8        string word="";
+9        while(i<n && s[i]!=' '){
+10            word+=s[i];
+11            i++;
+12        }
+13        reverse(word.begin(), word.end());
+14        if(word.length()>0){
+15            ans+=" " + word;
+16        }
+17       }
+18       return ans.substr(1);
+19    }
+20};
