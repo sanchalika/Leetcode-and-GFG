c973f5e349d28e355082b01a447133dd07415857
Sanchalika <sanchalika2019@gmail.com>
2026-03-06 23:12:21 +0530

    Add solution for Roman to Integer

--- LeetCode/Roman-to-Integer/Solution.cpp
+++ LeetCode/Roman-to-Integer/Solution.cpp
@@ -1,26 +1,34 @@
 1class Solution {
 2public:
-3    int char2num(char a) {
-4        switch (a) {
-5            case 'I': return 1;
-6            case 'V': return 5;
-7            case 'X': return 10;
-8            case 'L': return 50;
-9            case 'C': return 100;
-10            case 'D': return 500;
-11            case 'M': return 1000;
-12            default: return 0;
-13        }
-14    }
-15    int romanToInt(string s) {
-16        int summ=0;
-17        for(int i=0; i<s.length();i++){
-18            if(i+1 < s.length() && char2num(s[i]) < char2num(s[i+1])){
-19                summ-=char2num(s[i]);
-20            }else{
-21                summ+=char2num(s[i]);
-22            }
-23        }
-24        return summ;
-25    }
-26};
+3    int char2num(char ch) {
+4        switch (ch) {
+5        case 'I':
+6            return 1;
+7        case 'V':
+8            return 5;
+9        case 'X':
+10            return 10;
+11        case 'L':
+12            return 50;
+13        case 'C':
+14            return 100;
+15        case 'D':
+16            return 500;
+17        case 'M':
+18            return 1000;
+19        default:
+20            return 0;
+21        }
+22    }
+23    int romanToInt(string s) {
+24        int sum = 0;
+25        for (int i = 0; i < s.length(); i++) {
+26            if ((i + 1) < s.length() && char2num(s[i]) < char2num(s[i+1])) {
+27                sum -= char2num(s[i]);
+28            } else {
+29                sum += char2num(s[i]);
+30            }
+31        }
+32        return sum;
+33    }
+34};
