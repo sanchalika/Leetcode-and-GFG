93e5cc98bbc06921ef052b44ea09d4f9662d5ca8
Sanchalika <sanchalika2019@gmail.com>
2026-03-11 10:45:01 +0200

    Add solution for Odd Even Linked List

--- LeetCode/Odd-Even-Linked-List/Solution.java
+++ LeetCode/Odd-Even-Linked-List/Solution.java
@@ -10,19 +10,19 @@
 10 */
 11class Solution {
 12    public ListNode oddEvenList(ListNode head) {
-13        if(head==null){
+13        if (head == null) {
 14            return head;
 15        }
-16        ListNode odd=head;
-17        ListNode even=odd.next;
-18        ListNode evenHead=even;
-19        while(even!=null && even.next!=null){
-20            odd.next=even.next;
-21            odd=odd.next;
-22            even.next=odd.next;
-23            even=even.next;
+16        ListNode odd = head;
+17        ListNode even = odd.next;
+18        ListNode evenHead = even;
+19        while (even != null && even.next != null) {
+20            odd.next = even.next;
+21            odd = odd.next;
+22            even.next = odd.next;
+23            even = even.next;
 24        }
-25        odd.next=evenHead;
+25        odd.next = evenHead;
 26        return head;
 27    }
 28}
