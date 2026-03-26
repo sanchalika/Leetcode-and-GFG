b692eb5a1cd42cf33f19f763784415302e72871b
Sanchalika <sanchalika2019@gmail.com>
2026-03-26 18:47:30 +0200

    Add solution for Sum of Subarray Minimums

--- LeetCode/Sum-of-Subarray-Minimums/Solution.java
+++ LeetCode/Sum-of-Subarray-Minimums/Solution.java
@@ -1,28 +1,48 @@
-
-class Solution {
-public:
-    int sumSubarrayMins(vector<int>& arr) {
-        const int MOD = 1000000007;
-        stack<int> st;
-        long sumOfMinimums = 0;
-
-        for (int i = 0; i <= arr.size(); i++) {
-            while (!st.empty() && (i == arr.size() || arr[st.top()] >= arr[i])) {
-                int mid = st.top();
-                st.pop();
-                int leftBoundary = st.empty() ? -1 : st.top();
-                int rightBoundary = i;
-
-                long count = (mid - leftBoundary) * (rightBoundary - mid) % MOD;
-
-                sumOfMinimums += (count * arr[mid]) % MOD;
-                sumOfMinimums %= MOD;
-            }
-            st.push(i);
-        }
-
-        return static_cast<int>(sumOfMinimums);
-    }
-};
-
-
+1import java.util.*;
+2
+3class Solution {
+4    public int sumSubarrayMins(int[] arr) {
+5        int[] nse = nextSmallerEle(arr);
+6        int[] pse = prevSmallerEle(arr);
+7        int n = arr.length;
+8        long total = 0;
+9        int mod = 1_000_000_007;
+10
+11        for (int i = 0; i < n; i++) {
+12            long left = i - pse[i];
+13            long right = nse[i] - i;
+14            total = (total + (left * right % mod) * arr[i] % mod) % mod;
+15        }
+16        return (int) total;
+17    }
+18
+19    static int[] nextSmallerEle(int[] arr) {
+20        int n = arr.length;
+21        Stack<Integer> st = new Stack<>();
+22        int[] nse = new int[n];
+23
+24        for (int i = n - 1; i >= 0; i--) {
+25            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+26                st.pop();
+27            }
+28            nse[i] = st.isEmpty() ? n : st.peek();
+29            st.push(i);
+30        }
+31        return nse;
+32    }
+33
+34    static int[] prevSmallerEle(int[] arr) {
+35        int n = arr.length;
+36        Stack<Integer> st = new Stack<>();
+37        int[] pse = new int[n];
+38
+39        for (int i = 0; i < n; i++) {
+40            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+41                st.pop();
+42            }
+43            pse[i] = st.isEmpty() ? -1 : st.peek();
+44            st.push(i);
+45        }
+46        return pse;
+47    }
+48}
